bed34596b7917954e6e6930b7055e13fdd157fc5
Stealthhy7512 <kaanyazici7512@gmail.com>
2026-02-11 19:43:58 +0300

    feat: start server using handlers

--- urlshort/main.go
+++ urlshort/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"net/http"
 
+	"github.com/Stealthhy7512/gophercises/urlshort/handler"
 	"github.com/Stealthhy7512/gophercises/urlshort/router"
 )
 
+const yaml = `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution 
+`
+
 func main() {
-	r := router.SetupRouter()
+	provider, err := handler.YAMLHandler([]byte(yaml))
+	if err != nil {
+		panic(err)
+	}
+	r := router.SetupRouter(provider)
 
 	http.ListenAndServe("localhost:8080", r)
 }
