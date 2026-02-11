7772ff4b0d898c50435c435913b5eb5abb8f9707
Stealthhy7512 <kaanyazici7512@gmail.com>
2026-02-11 21:13:56 +0300

    refactor: update main to use cmd args and add example config files

--- urlshort/main.go
+++ urlshort/main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/Stealthhy7512/gophercises/urlshort/handler"
-	"github.com/Stealthhy7512/gophercises/urlshort/router"
+	"github.com/Stealthhy7512/gophercises/urlshort/cmd"
 )
 
-const yaml = `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution 
-`
-
 func main() {
-	provider, err := handler.YAMLHandler([]byte(yaml))
-	if err != nil {
-		panic(err)
-	}
-	r := router.SetupRouter(provider)
-
-	http.ListenAndServe("localhost:8080", r)
+	cmd.Execute()
 }
