2dc7ca93389c2849dd8a90efeca6b4c0950b4024
Stealthhy7512 <kaanyazici7512@gmail.com>
2026-02-11 18:57:19 +0300

    feat: implement router

--- urlshort/router/router.go
+++ urlshort/router/router.go
@@ -3,18 +3,21 @@ package router
 import (
 	"net/http"
 
+	"github.com/Stealthhy7512/gophercises/urlshort/handler"
 	"github.com/Stealthhy7512/gophercises/urlshort/utils"
 	"github.com/go-chi/chi/v5"
 )
 
 type JsonResponse map[string]string
 
-func SetupRouter() *chi.Mux {
+func SetupRouter(yh *handler.YAMLHandler) (*chi.Mux, error) {
 	r := chi.NewRouter()
 
 	r.Get("/", hello)
 
-	return r
+	yamlHandler, err := handler.YAMLHandler([]byte(yh))
+
+	return yamlHandler, err
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
