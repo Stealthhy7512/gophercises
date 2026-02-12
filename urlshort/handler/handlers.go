e9bde29067b106bd3b4a2b4f91a1d3a5f2cb66bd
Stealthhy7512 <kaanyazici7512@gmail.com>
2026-02-12 20:44:33 +0300

    feat: remove old file/map driven url logic and use new db based methods

--- urlshort/handler/handlers.go
+++ urlshort/handler/handlers.go
@@ -1,53 +1,32 @@
 package handler
 
 import (
-	"io"
-	"os"
-)
+	"context"
 
-type parsedQuery struct {
-	Path string `yaml:"path" json:"path"`
-	URL  string `yaml:"url" json:"url"`
-}
+	"github.com/Stealthhy7512/gophercises/urlshort/service"
+)
 
 type RedirectProvider interface {
 	GetURL(path string) (string, bool)
 }
 
-type MapHandler struct {
-	PathsToUrls map[string]string
+type URLHandler struct {
+	URLService service.URLService
+}
+
+func (u *URLHandler) ShortenURL(c context.Context, longURL string) (string, error) {
+	return u.URLService.ShortenURL(c, longURL)
 }
 
 // GetURL checks if the given path exists in the PathsToUrls map
 // and returns the corresponding URL and a boolean indicating if it was found.
-func (m *MapHandler) GetURL(path string) (string, bool) {
-	url, ok := m.PathsToUrls[path]
-	return url, ok
-}
+func (u *URLHandler) GetURL(path string) (string, bool) {
+	shortURL := path
 
-type decoderFunc func(r io.Reader, v any) error
-
-// QueryHandler reads the configuration from the provided file
-// and decodes it using the given decoder function (e.g., JSON or YAML).
-func QueryHandler(f *os.File, fn decoderFunc) (*MapHandler, error) {
-	pathUrls := []parsedQuery{}
-
-	if err := fn(f, &pathUrls); err != nil {
-		return nil, err
+	longURL, err := u.URLService.GetLongURL(context.Background(), shortURL)
+	if err != nil {
+		return "", false
 	}
 
-	pathMap := buildMap(pathUrls)
-
-	return &MapHandler{PathsToUrls: pathMap}, nil
-}
-
-// buildMap takes a slice of parsedQuery and constructs a map
-// where the keys are paths and the values are URLs.
-func buildMap(parsedQueries []parsedQuery) map[string]string {
-	paths := make(map[string]string)
-	for _, p := range parsedQueries {
-		paths[p.Path] = p.URL
-	}
-
-	return paths
+	return longURL, true
 }
