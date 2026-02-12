a0becdb97b8b88bf49fe813658cfceb43a6663c3
Stealthhy7512 <kaanyazici7512@gmail.com>
2026-02-12 20:42:51 +0300

    refactor: remove unnecessary tags from URL model

--- urlshort/model/url.go
+++ urlshort/model/url.go
@@ -7,8 +7,8 @@ import (
 )
 
 type URL struct {
-	ID        bson.ObjectID `bson:"_id,omitempty" json:"id,omitempty" yaml:"id,omitempty"`
-	ShortURL  string        `bson:"short_url" json:"short_url" yaml:"short_url"`
-	LongURL   string        `bson:"long_url" json:"long_url" yaml:"long_url"`
-	CreatedAt time.Time     `bson:"created_at" json:"created_at" yaml:"created_at"`
+	ID        bson.ObjectID `bson:"_id,omitempty" json:"-"`
+	ShortURL  string        `bson:"short_url"`
+	LongURL   string        `bson:"long_url"`
+	CreatedAt time.Time     `bson:"created_at"`
 }
