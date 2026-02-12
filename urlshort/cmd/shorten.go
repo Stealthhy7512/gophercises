429f8807ae48c60f5a5c179c9466d306a81d285d
Stealthhy7512 <kaanyazici7512@gmail.com>
2026-02-12 21:19:20 +0300

    refactor: change url flag to argument

--- urlshort/cmd/shorten.go
+++ urlshort/cmd/shorten.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Stealthhy7512/gophercises/urlshort/repository"
 	"github.com/Stealthhy7512/gophercises/urlshort/service"
@@ -11,18 +12,18 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-var longURL string
 var shortURL string
 
 var shortenCmd = &cobra.Command{
-	Use:   "shorten",
+	Use:   "shorten [url]",
 	Short: "Shorten a URL.",
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := utils.LoadMongoConfig()
 
 		client, err := mongo.Connect(options.Client().ApplyURI(cfg.URI))
 		if err != nil {
-			logger.Warn("Failed to connect to MongoDB", "error", err)
+			return err
 		}
 		defer func() {
 			if err := client.Disconnect(context.Background()); err != nil {
@@ -34,27 +35,24 @@ var shortenCmd = &cobra.Command{
 		col := db.Collection(cfg.Collection)
 		repo, err := repository.NewMongoURLRepository(col)
 		if err != nil {
-			logger.Warn("Failed to create URL repository", "error", err)
+			return err
 		}
 
 		s := service.NewURLService(repo)
 
 		c := context.Background()
 
+		longURL := args[0]
 		shortURL, err := s.ShortenURL(c, longURL)
 		if err != nil {
-			logger.Warn("Failed to shorten URL", "error", err)
-			return nil
+			return err
 		}
 
-		logger.Info("Shortened URL", "shortURL", shortURL)
+		fmt.Fprintln(cmd.OutOrStdout(), shortURL)
 		return nil
 	},
 }
 
 func init() {
-	shortenCmd.Flags().StringVarP(&longURL, "url", "u", "", "The URL to shorten")
-	shortenCmd.MarkFlagRequired("url")
-
 	rootCmd.AddCommand(shortenCmd)
 }
